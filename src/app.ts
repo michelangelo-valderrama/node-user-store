6aa38bbd332f293e97b9acd81f421f7a75e099a1
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-01-24 22:45:36 +0100

    routes and database

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 import { envs } from "./config/envs"
+import { MongoDatabase } from "./data"
 import { AppRoutes } from "./presentation/routes"
 import { Server } from "./presentation/server"
-
 ;(async () => {
   main()
 })()
 
-function main() {
-  const server = new Server({
-    port: envs.PORT,
-    routes: AppRoutes.routes,
-  })
-  server.start()
+async function main() {
+  try {
+    const db = MongoDatabase.connect({
+      dbName: envs.MONGO_DB_NAME,
+      mongoUrl: envs.MONGO_URL,
+    })
+
+    const server = new Server({
+      port: envs.PORT,
+      routes: AppRoutes.routes,
+    })
+    server.start()
+
+    const resp = await db
+    if (resp) console.log("[mongodb] connected...")
+  } catch (error) {
+    console.error("ERROR:", error)
+  }
 }
