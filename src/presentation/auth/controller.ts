f1cb7fb8f3aee64ac8f053724129cff79fb87206
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-01-25 23:15:25 +0100

    crear usuario y manejo de errores

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,18 +1,29 @@
 import { Request, Response } from "express"
-import { RegisterUserDto } from "../../domain"
+import { CustomError, RegisterUserDto } from "../../domain"
 import { AuthService } from "../services/auth.service"
 
 export class AuthController {
   // DI
   constructor(public readonly authService: AuthService) {}
 
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message })
+    }
+    console.log(`${error}`)
+    return res.status(500).json({ error: "Internal server error" })
+  }
+
   registerUser = (req: Request, res: Response) => {
     const [error, registerDto] = RegisterUserDto.create(req.body)
     if (error) return res.status(400).json(error)
 
-    this.authService.registerUser(registerDto!).then((resp) => {
-      return res.json(resp)
-    })
+    this.authService
+      .registerUser(registerDto!)
+      .then((resp) => {
+        return res.json(resp)
+      })
+      .catch((e) => this.handleError(e, res))
   }
 
   loginUser = (req: Request, res: Response) => {
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { UserModel } from "../../data"
 import { CustomError, RegisterUserDto } from "../../domain"
+import { UserEntity } from "../../domain"
 
 export class AuthService {
   // DI
@@ -8,6 +9,26 @@ export class AuthService {
   public async registerUser(registerUserDto: RegisterUserDto) {
     const existsUser = await UserModel.findOne({ email: registerUserDto.email })
     if (existsUser) throw CustomError.badRequest("Email already exist")
+
+    try {
+      const user = new UserModel(registerUserDto)
+      await user.save()
+
+      // Encriptar la contraseña
+
+      // JWT <--- para mantener la autentificación del usuario
+
+      // Email de confirmación
+
+      const { password, ...userEntity } = UserEntity.fromObject(user)
+
+      return {
+        user: userEntity,
+        token: "ABC",
+      }
+    } catch (e) {
+      throw CustomError.internalServerError(`${e}`)
+    }
 
     return "ok"
   }
