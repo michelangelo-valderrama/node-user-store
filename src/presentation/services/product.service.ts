b817d25a78ba457d549e3883f5a99c3ed68af90c
michelangelo-valderrama <michelangelovalderrama@gmail.com>
2024-02-03 10:50:23 +0100

    schemas to json

--- src/presentation/services/product.service.ts
+++ src/presentation/services/product.service.ts
@@ -27,7 +27,9 @@ export class ProductService {
         ProductModel.countDocuments(),
         ProductModel.find()
           .skip((page - 1) * limit)
-          .limit(limit),
+          .limit(limit)
+          .populate("user")
+          .populate("category"),
       ])
 
       return {
